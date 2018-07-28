462fc963dd2d9899673f82ad27a5839e1812b9e5
Saher AbdElaziz <saherabdelaziz@yahoo.com>
2018-07-28 17:28:14 +0200

    Add project files.

---
+++ moviesWebSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="moviesWebSite.MvcApplication" Language="C#" %>
---
+++ moviesWebSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(moviesWebSite.Startup))]
+namespace moviesWebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
