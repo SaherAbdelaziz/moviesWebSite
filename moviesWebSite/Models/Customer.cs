a8f1c40f87b6e479a1125d113d28e6a8bab31b9a
Saher AbdElaziz <saherabdelaziz@yahoo.com>
2018-07-29 08:33:59 +0200

    add membershipType Class

--- moviesWebSite/Models/Customer.cs
+++ moviesWebSite/Models/Customer.cs
@@ -10,5 +10,9 @@ namespace moviesWebSite.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
+
 }
