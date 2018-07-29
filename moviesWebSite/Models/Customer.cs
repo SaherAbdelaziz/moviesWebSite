8c1c72424843efe9a0bd9e52573412e098099401
Saher AbdElaziz <saherabdelaziz@yahoo.com>
2018-07-29 08:48:02 +0200

    change name limitation in customer class

--- moviesWebSite/Models/Customer.cs
+++ moviesWebSite/Models/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,9 @@ namespace moviesWebSite.Models
     public class Customer 
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
