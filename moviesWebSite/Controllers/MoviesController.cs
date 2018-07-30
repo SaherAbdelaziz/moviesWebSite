bff794bc8ea133195892d891d8036e6bed636059
Saher AbdElaziz <saherabdelaziz@yahoo.com>
2018-07-30 08:59:58 +0200

    add movies to dataBase and site

--- moviesWebSite/Controllers/MoviesController.cs
+++ moviesWebSite/Controllers/MoviesController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,13 +11,32 @@ namespace moviesWebSite.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
         }
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
 
+            return View(movie);
+        }
+        /*
         private IEnumerable<Movie> GetMovies()
         {
             return new List<Movie>
@@ -24,7 +44,7 @@ namespace moviesWebSite.Controllers
                 new Movie {Id = 1, Name = "Shrek"},
                 new Movie {Id = 2, Name = "Wall-e"}
             };
-        }
+        }*/
 
         /*
         public ActionResult Index(int? pageIndex, string sortBy)
