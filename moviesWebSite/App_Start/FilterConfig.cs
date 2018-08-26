e040c94c033e8558b08ae41793f417752ad1e2d6
Saher AbdElaziz <saherabdelaziz@yahoo.com>
2018-08-26 06:03:47 +0200

    anyone can visit pages

--- moviesWebSite/App_Start/FilterConfig.cs
+++ moviesWebSite/App_Start/FilterConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace moviesWebSite
 {
@@ -8,7 +7,7 @@ namespace moviesWebSite
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            filters.Add(new AuthorizeAttribute());
+           // filters.Add(new AuthorizeAttribute());
             filters.Add(new RequireHttpsAttribute());
         }
     }
